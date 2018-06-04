3cc31f7e27c1faa62e96dcbfacb1408e0a64e700
yuruisoft <417853832@qq.com>
2018-06-04 09:40:42 +0800

    ..

--- Yurui.Tools/src/Captcha/ImageProcess.cs
+++ Yurui.Tools/src/Captcha/ImageProcess.cs
@@ -347,9 +347,6 @@ namespace Yurui.Tools.Captcha
             return PBinary(src, v);
         }
 
-
-        #region 验证码
-
         /// <summary>
         /// 去除背景
         /// </summary>
@@ -407,76 +404,6 @@ namespace Yurui.Tools.Captcha
             img.UnlockBits(bdata);
             return img;
         }
-
-        /// <summary>
-        /// 得到灰度图像前景背景的临界值 最大类间方差法
-        /// </summary>
-        /// <param name="img">灰度图像</param>
-        /// <returns>灰度图像前景背景的临界值</returns>
-        public static int GetDgGrayValue(Image img)
-        {
-            Bitmap bmp = new Bitmap(img);
-            int[] pixelNum = new int[256];
-            int n, n1, n2;
-            int total;                              //total为总和，累计值
-            double m1, m2, sum, csum, fmax, sb;     //sb为类间方差，fmax存储最大方差值
-            int k, t, q;
-            int threshValue = 1;                      // 阈值
-
-            for (int i = 0; i < bmp.Width; i++)
-            {
-                for (int j = 0; j < bmp.Height; j++)
-                {
-                    //返回各个点的颜色，以RGB表示
-                    pixelNum[bmp.GetPixel(i, j).R]++;            //相应的直方图加1
-                }
-            }
-            //直方图平滑化
-            for (k = 0; k <= 255; k++)
-            {
-                total = 0;
-                for (t = -2; t <= 2; t++) //与附近2个灰度做平滑化
-                {
-                    q = k + t;
-                    if (q < 0)
-                        q = 0;
-                    if (q > 255)
-                        q = 255;
-                    total = total + pixelNum[q];
-                }
-                pixelNum[k] = (int)((float)total / 5.0 + 0.5);    // pixelNum[k] 的灰度值是前后5个点的平均值
-            }
-            //求阈值
-            sum = csum = 0.0;
-            n = 0;
-            //计算总的图象的点数和质量矩，为后面的计算做准备
-            for (k = 0; k <= 255; k++)
-            {
-                sum += (double)k * (double)pixelNum[k];     //x*f(x)质量矩，也就是每个灰度的值乘以其点数（归一化后为概率），sum为其总和
-                n += pixelNum[k];                       //n为图象总像素点数，归一化后就是累积概率
-            }
-
-            fmax = -1.0;                          //类间方差sb不可能为负，所以fmax初始值为-1不影响计算的进行
-            n1 = 0;
-            for (k = 0; k < 256; k++)                  //对每个灰度（从0到255）计算一次分割后的类间方差sb
-            {
-                n1 += pixelNum[k];                //n1为在当前阈值遍前景图象的点数
-                if (n1 == 0) { continue; }            //没有分出前景后景
-                n2 = n - n1;                        //n2为背景图象的点数
-                if (n2 == 0) { break; }               //n2为0表示全部都是后景图象，与n1=0情况类似，之后的遍历不可能使前景点数增加，所以此时可以退出循环
-                csum += (double)k * pixelNum[k];    //前景的“灰度的值*其像素点数”的总和
-                m1 = csum / n1;                     //m1为前景的平均灰度
-                m2 = (sum - csum) / n2;               //m2为背景的平均灰度
-                sb = (double)n1 * (double)n2 * (m1 - m2) * (m1 - m2);   //sb为类间方差
-                if (sb > fmax)                  //如果算出的类间方差大于前一次算出的类间方差
-                {
-                    fmax = sb;
-                    threshValue = k;              //k就是最佳阈值
-                }
-            }
-            return threshValue;
-        }
-
         /// <summary>
         /// 去除噪点
         /// </summary>
@@ -534,6 +461,9 @@ namespace Yurui.Tools.Captcha
 
             return img;
         }
+
+
+        #region 验证码
 
         /// <summary>
         /// 二值化处理
